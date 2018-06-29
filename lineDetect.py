7d0073e1995f2e3f7724a4dc9783a68c946e9616
soh92 <san4yone3@gmail.com>
2018-06-29 19:33:11 +0900

    dogフィルタを追加

--- lineDetect.py
+++ lineDetect.py
@@ -1,15 +1,155 @@
 # ライブラリ
 import cv2
 import numpy as np
+import os
+from matplotlib import pyplot as plt
 
-# 画像のエッジ検出関数
-def imageEdgeDetect(src):
+# ウィンドウ削除
+def windowControl():
+    # escキーを押すと終了
+    k = cv2.waitKey(0)
+    if k == 27:
+        # ウィンドウ削除
+        cv2.destroyAllWindows()
+
+# plot
+def plotHist(img):
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1, 2, 1)
+    ax2 = fig.add_subplot(1, 2, 2)
+    ax1.hist(img.ravel(), 256, [0, 256])
+    ax2.imshow(img)
+    plt.show()
+
+# ハフ変換
+def houghLinesOut(img, edges):
+    lines = cv2.HoughLines(edges, 1, np.pi / 180, 50)
+    for rho, theta in lines[0]:
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a * rho
+        y0 = b * rho
+        x1 = int(x0 + 1000 * (-b))
+        y1 = int(y0 + 1000 * (a))
+        x2 = int(x0 - 1000 * (-b))
+        y2 = int(y0 - 1000 * (a))
+
+        cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+# 確率的ハフ変換
+def houghLinesPOut(img, edges):
+    minLineLength = 10
+    maxLineGap = 20
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength, maxLineGap)
+    for x1, y1, x2, y2 in lines[0]:
+        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+
+# 精鋭化
+def sharpnening(src):
+    # カーネルサイズ
+    #kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]], np.float)
+    kernel = np.array([[0,-1,0], [-1,5,-1], [0,-1,0]], np.float)
+    # 畳み込み
+    conv = cv2.filter2D(src, -1, kernel)
+
+    return conv
+
+# lsd
+def lsd(src):
+    lsd = cv2.createLineSegmentDetector()
+    lines = lsd.detect(src)[0]
+    draw_line = lsd.drawSegments(src, lines)
+
+    return draw_line
+
+# バイラテラルフィルタの連続適用
+def straightBilateral(src, count):
+    for i in range(count):
+        # バイラテラルフィルタ(入力画像, 注目画素の領域, 画素値の差による重み, 画素間の距離差による重み)
+        src = cv2.bilateralFilter(src, 15, 20, 20)
+
+    return src
+
+# gausianのリストを作成
+def listOfGaus(count, gray, xy=3, sigma=1):
+    gaus_list = []
+    for i in range(count):
+        gaus_list.append(cv2.GaussianBlur(gray, (xy, xy), sigma + i))
+    return gaus_list
+
+# 差分のリストを作成
+def listOfDiff(gaus_list):
+    diff_list = []
+    for i in range(len(gaus_list)):
+        if i + 1 >= len(gaus_list):
+            print("終了")
+        else:
+            diff_list.append(gaus_list[i] - gaus_list[i + 1])
+    return diff_list
+
+
+def listOfHough(img, diff_list):
+    for i in range(len(diff_list)):
+        try:
+            houghLinesOut(img, diff_list[i])
+        except TypeError:
+            break
+
+    return img
+
+# 画像リストの書き出し
+def writeList(path, imgNo, img_list):
+    for i in range(len(img_list)):
+        # 画像の出力ディレクトリを作成
+        os.makedirs(path + str(imgNo), exist_ok=True)
+        cv2.imwrite(path + str(imgNo) + "/" + str(i) + ".jpg", img_list[i])
+
+
+# DoGフィルタ
+def dogDiff(gray, ksize, sigma):
+    # 標準偏差が異なる2つのガウシアン画像を算出
+    g1 = cv2.GaussianBlur(gray, ksize, sigma)
+    g2 = cv2.GaussianBlur(gray, ksize, sigma+1)
+    # 2つのガウシアン画像の差分を出力
+    return g1 - g2 , g2
+
+
+def DoB(gray, sigma1, sigma2):
+    # バイラテラルフィルタ(入力画像, 注目画素の領域, 画素値の差による重み, 画素間の距離差による重み)
+    b1 = cv2.bilateralFilter(gray, sigma1, 20, 20)
+    b2 = cv2.bilateralFilter(gray, sigma2, 20, 20)
+    # 2つのガウシアン画像の差分を出力
+    return b1 - b2
+
+# 画像のエッジ検出関数(LSD)
+def adaptiveThresh(src, imgNo):
+    # 画像読み込み
+    img = cv2.imread(src)
+    # グレースケール化
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # バイラテラルフィルタ(入力画像, 注目画素の領域, 画素値の差による重み, 画素間の距離差による重み)
+    bi = cv2.bilateralFilter(gray, 15, 20, 20)
+
+    # 適応的閾値処理（ガウシアン）
+    th = cv2.adaptiveThreshold(bi, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY, 11, 2)
+    # ハフ変換
+    houghLinesOut(img, th)
+
+    cv2.imwrite("../assets/adaptiveThresh/" + str(imgNo) + ".jpg", th)
+
+
+# 画像のエッジ検出関数(LSD+収縮)
+def lsdConst(src):
     # 画像読み込み
     img = cv2.imread(src)
     # グレースケール化
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     # バイラテラルフィルタ(入力画像, 注目画素の領域, 画素値の差による重み, 画素間の距離差による重み)
     bilateral = cv2.bilateralFilter(gray, 5, 50, 50)
+
     # LSDでエッジ検出
     lsd = cv2.createLineSegmentDetector()
     lines = lsd.detect(bilateral)[0]
@@ -28,97 +168,52 @@ def imageEdgeDetect(src):
     opening = cv2.morphologyEx(thresh, cv2.MORPH_ERODE, kernel)
 
     # ハフ変換
-    lines = cv2.HoughLines(opening, 1, np.pi / 180, 200)
-    for rho, theta in lines[0]:
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a * rho
-        y0 = b * rho
-        x1 = int(x0 + 1000 * (-b))
-        y1 = int(y0 + 1000 * (a))
-        x2 = int(x0 - 1000 * (-b))
-        y2 = int(y0 - 1000 * (a))
+    houghLinesOut(img, opening)
 
-        cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
-    # print(lines[0][0][0])
     # ウィンドウ生成＆表示
-    cv2.namedWindow("img", cv2.WINDOW_GUI_EXPANDED)
-    cv2.namedWindow("lsd", cv2.WINDOW_GUI_EXPANDED)
     cv2.imshow("img", img)
     cv2.imshow("lsd", img)
 
-    # escキーを押すと終了
-    k = cv2.waitKey(0)
-    if k == 27:
-        # ウィンドウ削除
-        cv2.destroyAllWindows()
+    # ウィンドウ操作
+    windowControl()
 
-# ビデオキャプチャのエッジ検出関数
-def videoEdgeDetect():
-    cap = cv2.VideoCapture(0)
-    while True:
-        ret, frame = cap.read()
-        # グレースケール化
-        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        # バイラテラルフィルタでエッジを残して平滑化
-        bilateral = cv2.bilateralFilter(gray, 15, 75, 75)
-        # LSDでエッジ検出
-        lsd = cv2.createLineSegmentDetector()
-        lines = lsd.detect(bilateral)[0]
-        drawn_img = lsd.drawSegments(bilateral, lines)
-
-        try:
-            # ウィンドウ生成＆表示
-            cv2.namedWindow("lsd", cv2.WINDOW_GUI_EXPANDED)
-            cv2.imshow("lsd", drawn_img)
-        except TypeError:
-            print("error")
-
-        # escキーを押すと終了
-        k = cv2.waitKey(5)
-        if k == 27:
-            break
-
-    # ウィンドウ削除
-    cv2.destroyAllWindows()
-    cap.release()
-
-
-def canny(src):
+def otsuOnly(src, imgNo):
     # 画像読み込み
     img = cv2.imread(src)
+
     # グレースケール化
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    # canny
-    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
 
-    lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
-    for rho, theta in lines[0]:
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a * rho
-        y0 = b * rho
-        x1 = int(x0 + 1000 * (-b))
-        y1 = int(y0 + 1000 * (a))
-        x2 = int(x0 - 1000 * (-b))
-        y2 = int(y0 - 1000 * (a))
+    # otsuThresh
+    ret, th = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-        cv2.line(img, (x1, y1), (x2, y2),(0,0,255),2)
+    # ハフ変換
+    houghLinesOut(img, th)
 
-    # ウィンドウ生成＆表示
-    cv2.namedWindow("img", cv2.WINDOW_GUI_EXPANDED)
-    cv2.namedWindow("lsd", cv2.WINDOW_GUI_EXPANDED)
-    cv2.imshow("img", img)
-    cv2.imshow("lsd", edges)
+    cv2.imwrite("../assets/otsuOnly/otsu/" + str(imgNo) + ".jpg", th)
+    cv2.imwrite("../assets/otsuOnly/line/" + str(imgNo) + ".jpg", img)
 
-    # escキーを押すと終了
-    k = cv2.waitKey(0)
-    if k == 27:
-        # ウィンドウ削除
-        cv2.destroyAllWindows()
+def dog(src, imgNo):
+    houghList = []
+    # 画像読み込み
+    img = cv2.imread(src)
+
+    # グレースケール化
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    gausList = listOfGaus(10, gray)
+    diffList = listOfDiff(gausList)
+    houghList.append(listOfHough(img, diffList))
+    
+    writeList("../assets/dog/gaus/", imgNo, gausList)
+    writeList("../assets/dog/diff/", imgNo, diffList)
+    writeList("../assets/dog/hough/", imgNo, houghList)
 
 
 if __name__ == "__main__":
-    src = "img_in/5.jpg"
-    imageEdgeDetect(src)
+    # src = "../assets/img_in/17.jpg"
+    # dogFilter(src)
 
+    for imgNo in range(11, 20):
+        src = "../assets/img_in/" + str(imgNo) + ".jpg"
+        dog(src, imgNo)
